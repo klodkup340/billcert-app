275383a2aaad4547b522dc2117bacb567ad21cb5
Sagun Phetkaew <klodklod@Saguns-MacBook-Pro.local>
2021-01-24 13:11:24 +0700

    add staging environment test

--- amplify/backend/function/mainlambda/src/app.js
+++ amplify/backend/function/mainlambda/src/app.js
@@ -1,17 +1,7 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const axios = require('axios');
 
 // declare a new express app
 var app = express()
@@ -25,24 +15,15 @@ app.use(function (req, res, next) {
   next()
 });
 
-
-/**********************
- * Example get method *
- **********************/
-
-app.get('/item', function (req, res) {
-  const data = [{ name: 'klod' }, { name: 'por' }, { name: 'ploi' }, { name: 'koon' }]
-  res.json({ success: 'get call succeed!', url: req.url, data });
+app.get('/item', async (req, res) => {
+  const data = await axios.get('https://swapi.dev/api/people/');
+  res.json({ success: 'get call succeed!', url: req.url, result: data.data.results });
 });
 
 app.get('/item/*', function (req, res) {
   // Add your code here
   res.json({ success: 'get call succeed!', url: req.url });
 });
-
-/****************************
-* Example post method *
-****************************/
 
 app.post('/item', function (req, res) {
   // Add your code here
@@ -54,10 +35,6 @@ app.post('/item/*', function (req, res) {
   res.json({ success: 'post call succeed!', url: req.url, body: req.body })
 });
 
-/****************************
-* Example put method *
-****************************/
-
 app.put('/item', function (req, res) {
   // Add your code here
   res.json({ success: 'put call succeed!', url: req.url, body: req.body })
@@ -67,10 +44,6 @@ app.put('/item/*', function (req, res) {
   // Add your code here
   res.json({ success: 'put call succeed!', url: req.url, body: req.body })
 });
-
-/****************************
-* Example delete method *
-****************************/
 
 app.delete('/item', function (req, res) {
   // Add your code here
@@ -86,7 +59,4 @@ app.listen(3000, function () {
   console.log("App started")
 });
 
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
 module.exports = app
